f71e08acf5e0f8c65cc0a2005c7bfe8b10ab0a1f
josue josuerushanika <100031798+josuerushanika@users.noreply.github.com>
2023-03-21 16:59:32 +0200

    Add route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Missions from './components/Mission';
+import Navbar from './components/Navbar';
+import Profile from './components/Profile';
+import Rockets from './components/Rockets';
 
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Rockets />} />
+        <Route path="/Missions" element={<Missions />} />
+        <Route path="/Profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
