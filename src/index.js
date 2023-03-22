fb20a3d0fccb85b45a4a4a45f6b8fa6302069688
Frempong <samankwaa.frempong@gmail.com>
2023-03-22 17:28:55 +0000

    Fix Linter errors

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/store';
 import reportWebVitals from './reportWebVitals';
-import store from './redux/store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
