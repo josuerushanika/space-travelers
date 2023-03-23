b7f731e88e90790463247031e4bade0a38b577af
josue josuerushanika <100031798+josuerushanika@users.noreply.github.com>
2023-03-23 12:20:59 +0200

    Update function

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,7 +1,12 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { Button } from 'react-bootstrap';
+import { reserveRocket } from '../redux/rockets/rocketSlice';
 
 const Rockets = () => {
   const rockets = useSelector((state) => state.rockets.rocketListData);
+  const dispatch = useDispatch();
+
+  const bookHandler = (id) => dispatch(reserveRocket(id));
 
   return (
     <>
@@ -12,6 +17,9 @@ const Rockets = () => {
               <img src={rocket.flickr_image[0]} alt="img" />
               <h2>{rocket.name}</h2>
               <p>{rocket.description}</p>
+              <Button onClick={() => bookHandler(rocket.id)}>
+                Reserve Rocket
+              </Button>
             </div>
           ))
         )}
