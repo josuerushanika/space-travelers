98c9af4bc344460548570c5cf0270822484c5412
Frempong <samankwaa.frempong@gmail.com>
2023-03-23 03:43:19 +0000

    Display joined Missions in Profile component

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,17 @@
-const Profile = () => (
-  <div>Profile</div>
-);
+import { useSelector } from 'react-redux';
+
+const Profile = () => {
+  const { missions } = useSelector((state) => state.missions);
+  return (
+    <div>
+      Profile
+      <h1>My Missions</h1>
+      <div className="missionBox">
+        {missions.filter((mission) => mission.joined)
+          .map((mission) => <div key={mission.mission_id} className="joinedMission">{mission.mission_name}</div>)}
+      </div>
+    </div>
+  );
+};
 
 export default Profile;
