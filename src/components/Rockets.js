4f43f05675a462f1c402bce6d4d5b31c9a609e39
josue josuerushanika <100031798+josuerushanika@users.noreply.github.com>
2023-03-23 15:36:09 +0200

    Add Cancel button and badge

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,12 +1,17 @@
 import { useSelector, useDispatch } from 'react-redux';
-import { Button } from 'react-bootstrap';
-import { reserveRocket } from '../redux/rockets/rocketSlice';
+import { Button, Badge } from 'react-bootstrap';
+import { reserveRocket, cancelRocket } from '../redux/rockets/rocketSlice';
 
 const Rockets = () => {
   const rockets = useSelector((state) => state.rockets.rocketListData);
   const dispatch = useDispatch();
 
-  const bookHandler = (id) => dispatch(reserveRocket(id));
+  const bookHandler = (id, reserve) => {
+    if (reserve) {
+      dispatch(cancelRocket(id));
+    }
+    dispatch(reserveRocket(id));
+  };
 
   return (
     <>
@@ -16,9 +21,28 @@ const Rockets = () => {
             <div key={rocket.id}>
               <img src={rocket.flickr_image[0]} alt="img" />
               <h2>{rocket.name}</h2>
-              <p>{rocket.description}</p>
-              <Button onClick={() => bookHandler(rocket.id)}>
-                Reserve Rocket
+              <p>
+
+                {rocket.reserved && (
+                <Badge
+                  bg="success"
+                  className="p-2"
+                  style={{ marginRight: '1vw' }}
+                >
+                  Reserved
+                </Badge>
+                )}
+
+                {rocket.description}
+
+              </p>
+              <Button
+                type="button"
+                variant={rocket.reserved ? ('outline-secondary') : ('btn btn-primary')}
+                size="md"
+                onClick={() => bookHandler((rocket.id))}
+              >
+                {rocket.reserved ? 'Cancel Reservation' : 'Reserve Rocket'}
               </Button>
             </div>
           ))
