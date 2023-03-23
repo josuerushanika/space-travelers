85ad50b00a7a338d21243cbc24743f98bf9c42ad
Frempong <samankwaa.frempong@gmail.com>
2023-03-23 04:29:19 +0000

    Update Profile component with local state to show message for no joined mission

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,14 +1,33 @@
+import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 const Profile = () => {
   const { missions } = useSelector((state) => state.missions);
+  const [joinedMissions, setJoinedMissions] = useState([]);
+
+  useEffect(() => {
+    const filteredMissions = missions.filter((mission) => mission.joined);
+    setJoinedMissions(filteredMissions);
+  }, [missions]);
+
+  if (!joinedMissions.length) {
+    return (
+      <div className="missionBox">
+        <h1>My Missions</h1>
+        <div className="noJoined">No Missions Joined</div>
+      </div>
+    );
+  }
   return (
     <div>
       Profile
       <h1>My Missions</h1>
       <div className="missionBox">
-        {missions.filter((mission) => mission.joined)
-          .map((mission) => <div key={mission.mission_id} className="joinedMission">{mission.mission_name}</div>)}
+        {joinedMissions.map((mission) => (
+          <div key={mission.mission_id} className="joinedMission">
+            {mission.mission_name}
+          </div>
+        ))}
       </div>
     </div>
   );
